a175e19e0bdc87b14beb19798fa4d58a6b566b37
meliodas113 <anupam.fas27@gmail.com>
2024-07-10 01:59:23 +0530

    settlement added

---
+++ src/components/hooks/useSettleMarket.ts
@@ -0,0 +1,75 @@
+import {
+  useAccount,
+  useContract,
+  useContractWrite,
+} from "@starknet-react/core";
+import { useMemo } from "react";
+import { CallData, byteArray, cairo } from "starknet";
+import { CONTRACT_ADDRESS } from "../helpers/constants";
+import abi from "../../abi/ContractABI.json";
+
+interface Data {
+  category: string;
+  marketId: BigInt;
+  outcome: number;
+}
+
+function useSettleMarket(marketData: Data) {
+  const { address } = useAccount();
+
+  const { contract } = useContract({
+    address: CONTRACT_ADDRESS,
+    abi: abi,
+  });
+  const settleNormalMarketCalls = useMemo(() => {
+    if (!contract || !address || !marketData.category || !marketData.marketId)
+      return [];
+    return [
+      contract.populateTransaction["settle_market"]!(
+        marketData.marketId,
+        BigInt(marketData.outcome)
+      ),
+    ];
+  }, [contract, address, marketData]);
+
+  const settleSportsMarketCalls = useMemo(() => {
+    if (!contract || !address || !marketData.category || !marketData.marketId)
+      return [];
+    return [
+      contract.populateTransaction["settle_sports_market_manually"]!(
+        marketData.marketId,
+        BigInt(marketData.outcome)
+      ),
+    ];
+  }, [contract, address, marketData]);
+
+  const settleCryptoMarketCalls = useMemo(() => {
+    if (!contract || !address || !marketData.category || !marketData.marketId)
+      return [];
+    return [
+      contract.populateTransaction["settle_crypto_market_manually"]!(
+        marketData.marketId,
+        BigInt(marketData.outcome)
+      ),
+    ];
+  }, [contract, address, marketData]);
+
+  const { writeAsync, data, error, isError, isSuccess, isPending } =
+    useContractWrite({
+      calls:
+        marketData.category == "crypto"
+          ? settleCryptoMarketCalls
+          : marketData.category == "sports"
+          ? settleSportsMarketCalls
+          : settleNormalMarketCalls,
+    });
+
+  const settleMarket = async () => {
+    console.log(marketData);
+    await writeAsync();
+  };
+
+  return { settleMarket, data, isError, isSuccess, isPending, error };
+}
+
+export default useSettleMarket;
