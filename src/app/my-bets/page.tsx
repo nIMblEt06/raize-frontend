8b820e57eecb7a0a6c011e1573a7a4fa097420a6
meliodas113 <anupam.fas27@gmail.com>
2024-07-24 23:06:19 +0530

    removed my bets

--- src/app/my-bets/page.tsx
+++ src/app/my-bets/page.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import "./bets.scss";
-import BetDetails from "@/components/MyBets"; 
+import BetDetails from "@/components/MyBets";
 
 function page() {
-  return (
-    <div className='Contents'>
-      <BetDetails />
-    </div>
-  );
+  return <div className="Contents">{/* <BetDetails /> */}</div>;
 }
 
 export default page;
