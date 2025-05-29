5c51123322cd73234fc560ebf3ac964f8d237cfb
Devansh goel <f20220457@pilani.bits-pilani.ac.in>
2025-05-29 13:56:41 +0530

    made changes

--- src/components/Header/WalletButtons/index.tsx
+++ src/components/Header/WalletButtons/index.tsx
@@ -38,7 +38,6 @@ const WalletButtons: NextPage<Props> = () => {
     handleClose,
     handleClick,
   } = useDropdown();
-
   const shortenedAddress = useMemo(() => {
     if (!address) return "";
     return `${address.slice(0, 6)}...${address.slice(-4)}`;
